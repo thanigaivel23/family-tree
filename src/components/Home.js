89a6fbf02488546ae0dda9dd4db8f47b3a5293ab
thanigaivel23 <thanigaivel23yaazh.com>
2024-11-11 21:01:22 +0530

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,58 +1,90 @@
 import React from 'react'
-import familyTree from '../img/familytree.jpeg'
+import logo from '../img/logo.png'
+import hero from '../img/hero.jpg'
+import Vector from '../img/Vector.png'
+import first from '../img/first.jpeg'
+import second from '../img/second.jpeg'
+import thrid from '../img/thrid.jpeg'
 
 const Home = () => {
     return (
         <>
-            <main className='tw-bg-[#f5f7f5] tw-px-20 poppins'>
-                <section className='tw-py-10'>
-                    <h1 className='tw-text-[#90ee91] tw-text-2xl tw-py-2 tw-font-medium'> Discover Your Roots, Connect with Your Legacy </h1>
-                    <p className='tw-pb-5 tw-pl-5'> Explore your family tree, preserve your history, and connect with loved ones across generations </p>
-                    <button className='tw-mx-5 tw-bg-[#90ee91] tw-text-white tw-px-3 tw-py-2 tw-rounded-md hover:tw-bg-[#78e67a] tw-duration-200'>Start Your Journey</button>
-                    <button className='tw-mx-5 tw-bg-[#90ee91] tw-text-white tw-px-3 tw-py-2 tw-rounded-md hover:tw-bg-[#78e67a] tw-duration-200'>Create Your Family Tree</button>
-                </section>
+            {/* header */}
+            <section className='tw-flex tw-items-center tw-px-24 tw-pt-5 tw-pb-2'>
+                <main className='tw-w-[50%]'>
+                    <img src={logo} alt="" className='tw-w-40' />
+                </main>
+                <main className='tw-w-[50%] tw-flex tw-items-center tw-gap-x-24 tw-text-[#63a731]'>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Features</p>
+                    <p className='tw-text-black tw-bg-[#63a731] tw-border-4 tw-border-black tw-px-10 tw-font-medium tw-py-1 tw-rounded-full'>Add Members</p>
+                </main>
+            </section>
 
-                <div className='tw-flex tw-justify-center'>
-                    <img src={familyTree} width={600} className='tw-rounded-md ' alt="" />
+            {/* hero section */}
+            <section className='tw-flex tw-items-center tw-bg-[#f9f9f9]'>
 
-                </div>
+                <main className='tw-w-[70%] tw-pl-28'>
 
-                <section className='tw-pt-10'>
-                    <h1 className='tw-text-[#90ee91] tw-text-2xl tw-py-2 tw-font-medium'> Why Choose Our Family Tree Platform? </h1>
-                    <p className='tw-pb-5 tw-pl-5'><span className='tw-font-medium tw-text-[#522420] tw-text-lg '>Build & Expand Your Tree :</span> Easily add family members, photos, and milestones.</p>
-                    <p className='tw-pb-5 tw-pl-5'><span className='tw-font-medium tw-text-[#522420] tw-text-lg '>Discover Hidden Stories :</span> Uncover fascinating details with our historical records database.</p>
-                    <p className='tw-pb-5 tw-pl-5'><span className='tw-font-medium tw-text-[#522420] tw-text-lg '>Collaborate with Family :</span> Invite family members to contribute and share memories.</p>
-                </section>
+                    <p className='tw-text-[#e25953] tw-text-5xl tw-font-medium'>
+                        Discover Your Roots, <br />
+                        Connect with Your Legacy
+                    </p>
 
-                <section className='tw-pt-10'>
-                    <h1 className='tw-text-[#90ee91] tw-text-2xl tw-py-2 tw-font-medium'> How It Works </h1>
-                    <p className='tw-pb-5 tw-pl-5'><span className='tw-font-medium tw-text-[#522420] tw-text-lg '>Create an Account :</span> Sign up and start building your family tree in minutes.</p>
-                    <p className='tw-pb-5 tw-pl-5'><span className='tw-font-medium tw-text-[#522420] tw-text-lg '>Add Family Members :</span> Add relatives, photos, and key moments to enrich your tree.</p>
-                    <p className='tw-pb-5 tw-pl-5'><span className='tw-font-medium tw-text-[#522420] tw-text-lg '>Explore & Share :</span> Trace your lineage and share your tree with others.</p>
-                </section>
+                    <p className='tw-text-[#63a731] tw-font-medium tw-text-2xl tw-mt-7'>
+                        Explore your family tree, preserve your history, and connect with
+                        loved ones across generations.
+                    </p>
 
-                <section className='tw-py-16 '>
-                    <h1 className='tw-text-black tw-text-2xl tw-py-2 tw-font-medium tw-text-center tw-pb-10'> What Our Users Say </h1>
-                    <main className='tw-flex tw-justify-center tw-gap-x-10'>
-                        <div className='tw-shadow-md tw-border tw-w-[30%] tw-rounded-lg tw-p-5'>
-                            <p class="tw-font-bold tw-text-[#333] tw-text-lg">Prakash</p>
-                            <p class="tw-text-[#777]"> discovered family connections I never knew existed!</p>
-                        </div> <div className='tw-shadow-md tw-border tw-w-[30%] tw-rounded-lg tw-p-5'>
-                            <p class="tw-font-bold tw-text-[#333] tw-text-lg">Thanigaivel</p>
-                            <p class="tw-text-[#777]">The perfect tool to preserve our family legacy for future generations</p>
-                        </div>
-                    </main>
-                </section>
 
-                <section className='tw-py-10'>
-                    <h1 className='tw-text-[#90ee91] tw-text-4xl tw-py-2 tw-font-medium tw-text-center'> Start Building Your Family’s Legacy Today </h1>
-                    <div className=' tw-flex tw-justify-center tw-mt-10 tw-gap-x-20'>
-                        <button className=' tw-bg-[#90ee91] tw-text-white tw-px-3 tw-py-2 tw-rounded-md hover:tw-bg-[#78e67a] tw-duration-200'>Get Started Now</button>
-                        <button className=' tw-bg-[#90ee91] tw-text-white tw-px-3 tw-py-2 tw-rounded-md hover:tw-bg-[#78e67a] tw-duration-200'>Join the Community</button>
+                    <div className='tw-flex tw-gap-x-40'>
+                        <button className='tw-mt-14 tw-flex tw-items-center tw-text-white tw-bg-[#63a731] tw-rounded-xl tw-py-4 tw-px-7  tw-text-sm tw-font-medium tw-gap-x-9'>
+                            Start Your Journey
+                            <img src={Vector} alt="" />
+                        </button>
+
+                        <button className='tw-mt-14 tw-flex tw-items-center tw-text-white tw-bg-[#e25953] tw-rounded-xl tw-py-4 tw-px-7  tw-text-sm tw-font-medium tw-gap-x-9'>
+                            Create Your Family Tree
+                            <img src={Vector} alt="" />
+                        </button>
                     </div>
-                </section>
 
-            </main>
+                </main>
+                <main className=''>
+                    <img src={hero} alt="" className='' />
+                </main>
+            </section>
+
+            {/* 2nd section */}
+            <section className='tw-px-20'>
+                <p className='tw-text-center tw-font-medium tw-text-4xl tw-py-5'>Why Choose Our Family Tree Platform?</p>
+                <main className=' tw-grid tw-grid-cols-3 tw-gap-x-28'>
+
+                    <div className='tw-border tw-rounded-xl tw-pt-5 tw-shadow-md'>
+                        <h1 className='tw-text-center tw-font-medium tw-text-xl'>Build & Expand Your Tree</h1>
+                        <p className='tw-px-14 tw-pt-4'>Easily add family members, photos, and milestones</p>
+                        <img src={first} alt="" />
+                    </div>
+
+                    <div className='tw-border tw-rounded-xl tw-pt-5 tw-shadow-md'>
+                        <h1 className='tw-text-center tw-font-medium tw-text-xl'>Discover Hidden Stories</h1>
+                        <p className='tw-px-14 tw-pt-4'>Uncover fascinating details with our historical records database.</p>
+                        <img src={second} alt="" />
+                    </div>
+
+                    <div className='tw-border tw-rounded-xl tw-pt-5 tw-shadow-md'>
+                        <h1 className='tw-text-center tw-font-medium tw-text-xl'>Collaborate with Family</h1>
+                        <p className='tw-px-14 tw-pt-4'>Invite family members to contribute and share memories.</p>
+                        <img src={thrid} alt="" className='tw-w-[70%] tw-mx-auto tw-mt-10'/>
+                    </div>
+                    
+                    
+
+
+                </main>
+
+            </section>
         </>
     )
 }
